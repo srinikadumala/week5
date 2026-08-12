5aec2e605e3d334d87965477b5d49e49097e5a99
srinikadumala <srinika1238@gmail.com>
2026-08-12 10:28:54 +0530

    Add TestFactorial and manifest

---
+++ TestFactorial.java
@@ -0,0 +1,14 @@
+public class TestFactorial {
+
+    public static void main(String[] args) {
+
+        int result = Factorial.calculate(5);
+
+        if (result == 120) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            System.exit(1);
+        }
+    }
+}
